d84eaa6146f04927cc6843604ea6f8abca787aeb
Josh Rutherford <joshrutherford@Joshs-MacBook-Air.local>
2015-11-05 19:57:35 -0800

    start on player matching, method to set player game

--- server/methods.js
+++ server/methods.js
@@ -1,9 +1,39 @@
 Meteor.methods({
 	createGame: function() {
-		if( Games.findOne( { unfilled: true } ) ) {
+		// TODO:
+		// ~~~~~
+		// * Fix issue where one user
+		//   can become his own opponent
+		//
+		// * Add /player1 and /player2 routes
+		//
+
+		var unfilledGame = Games.findOne( { unfilled: true } );
+
+		// Player 1:
+		if( unfilledGame ) {
 			console.log( 'adding to unfilled game' );
-		} else {
+
+			Games.update( unfilledGame._id, {
+				$set: {
+					player2: this.userId,
+					unfilled: false
+				}
+			});
+
+			FlowRouter.go( '/player1' );
+		}
+
+		// Player 2:
+		else {
 			console.log( 'no unfilled games, create a new one' );
+
+			Games.insert( {
+				player1: this.userId,
+				unfilled: true
+			});
+
+			FlowRouter.go( '/player2' );
 		}
 	}
 });
